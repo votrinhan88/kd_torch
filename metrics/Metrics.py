8568c6e4184f71dd32540a55f69b78c09c60d731
votrinhan88 <votrinhan88@gmail.com>
2022-12-12 22:03:45 +1100

    Add BinaryAccuracy (metric)

--- metrics/Metrics.py
+++ metrics/Metrics.py
@@ -57,6 +57,27 @@ class CategoricalAccuracy(Metric):
         self.num_observed = torch.zeros(1)
         self.value = (self.label == self.pred_label).sum()/self.num_observed
 
+class BinaryAccuracy(Metric):
+    def __init__(self):
+        self.reset()
+    
+    def update(self, label:torch.Tensor, prediction:torch.Tensor):
+        # `prediction` must be probability
+        pred_label = (prediction >= 0.5).to(dtype=torch.float)
+        self.num_observed += label.shape[0]
+
+        self.label = torch.cat(tensors=[self.label, label], dim=0)
+        self.pred_label = torch.cat(tensors=[self.pred_label, pred_label], dim=0)
+
+        self.value = (self.label == self.pred_label).sum()/self.num_observed
+        return self.value
+
+    def reset(self):
+        self.label = torch.Tensor()
+        self.pred_label = torch.Tensor()
+        self.num_observed = torch.zeros(1)
+        self.value = (self.label == self.pred_label).sum()/self.num_observed
+
 if __name__ == '__main__':
     mean = Mean()
     accuracy = CategoricalAccuracy()
