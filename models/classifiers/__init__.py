781eb91f4763ce0ed40ae0ac76f59738f347f3ea
votrinhan88 <votrinhan88@gmail.com>
2022-12-18 20:36:29 +1100

    Add ResNet

--- models/classifiers/__init__.py
+++ models/classifiers/__init__.py
@@ -1,6 +1,7 @@
 from .alexnet import AlexNet
 from .hintonnet import HintonNet
 from .lenet5 import LeNet5
+from .resnet import ResNet18, ResNet34
 from .utils import ClassifierTrainer
 
 del alexnet
