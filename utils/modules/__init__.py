8de313b51439ee8978fa760cc6c9127e19da2c7f
votrinhan88 <votrinhan88@gmail.com>
2022-12-18 20:36:18 +1100

    Add Identity module

--- utils/modules/__init__.py
+++ utils/modules/__init__.py
@@ -1,7 +1,9 @@
 from .concatenate import Concatenate
+from .identity import Identity
 from .onehot_encoding import OneHotEncoding
 from .reshape import Reshape
 
 del concatenate
+del identity
 del onehot_encoding
 del reshape
